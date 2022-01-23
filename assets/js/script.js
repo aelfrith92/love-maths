762cbdc84f686005770198eb63b021e71dec5781
Alfre <fred.morieri@gmail.com>
2022-01-23 16:34:11 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('connected');
+function runGame() {
+    
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function intcrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
