da15dbbcc55f7e31f89c87cad554b0506b5fc45e
Alfre <fred.morieri@gmail.com>
2022-01-23 22:45:54 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+//Waiting for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+/*The following Event Listener contains an anonymous function which executes when the event occurs*/
+document.addEventListener('DOMContentLoaded', function() {
+    //recupera HTML collection di bottoni in formato array
+    let buttons = document.querySelectorAll('button');
+    //iterazione nell'array appena recuperato
+    for (let button of buttons) {
+        //button è la variabile che andremo a controllare, una per una, per due ragioni:
+        // 1. le verra' associata un listener; 2. ne verra' estrapolato l'attributo personalizzato
+        // per controllare quale sia stato effettivamente premuto.
+        button.addEventListener('click', function(){
+            if(this.getAttribute('data-type') === 'submit') {
+                alert('you clicked submit');
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
     
 }
@@ -21,5 +44,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
